1c911ad39fa43125032b0e69cfecd2af62b7f9c3
anilalperr <anilalper2641@gmail.com>
2023-01-13 01:27:19 +0300

    map commit

--- web_page.js
+++ web_page.js
@@ -1,19 +1,18 @@
 $(document).ready(function(){
     require(["esri/config", "esri/Map", "esri/views/MapView"], function(esriConfig, Map, MapView) {
         esriConfig.apiKey = "AAPKf56f41c3c3fe4987b2f4c84072150f20sI3IXY6IaRdrxOP0FD5wRpYbFqNbA8y8g9PtOAn8D8QEPwmKZb1Mdp0sexb1frQo";
+        
         const map = new Map({
             basemap: "arcgis-topographic" // Basemap layer service
           });
-    });
-
-    /*
-    const view = new MapView({
+    
+        const view = new MapView({
         map: map,
         center: [-118.805, 34.027], // Longitude, latitude
         zoom: 13, // Zoom level
         container: "viewDiv" // Div element
-      });
-      */
+        });
+    });
 
     $("#home").click(function(){
         $(".texts").hide();
